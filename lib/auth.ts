f856afcd1d810a6de86fac699d087abd63aa088e
Thabiso Pheko <tabs150@gmail.com>
2025-08-12 17:15:55 +0200

    feat: add a basic implementation of email verification

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,17 +1,32 @@
-import { db } from "@/db/drizzle";
-import { schema } from "@/db/schema";
+import VerifyEmail from '@/components/emails/verify-email';
+import { db } from '@/db/drizzle';
+import { schema } from '@/db/schema';
 
-import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { nextCookies } from "better-auth/next-js";
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { nextCookies } from 'better-auth/next-js';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      resend.emails.send({
+        from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+        to: user.email,
+        subject: 'Verify your email',
+        react: VerifyEmail({ username: user.name, verifyUrl: url }),
+      });
+    },
+  },
   database: drizzleAdapter(db, {
-    provider: "pg",
+    provider: 'pg',
     schema,
   }),
-    emailAndPassword: {
+  emailAndPassword: {
     enabled: true,
   },
-  plugins: [nextCookies()]
+  plugins: [nextCookies()],
 });
