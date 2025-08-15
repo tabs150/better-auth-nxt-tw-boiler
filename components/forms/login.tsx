8cb09f35d78381582953999fb3dadb8bb7879a34
Thabiso Pheko <tabs150@gmail.com>
2025-08-15 19:48:24 +0200

    chore: test react-hook-form with zod

--- components/forms/login.tsx
+++ components/forms/login.tsx
@@ -1,82 +1,88 @@
-"use client"
+'use client';
 
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Input } from "../ui/input";
-import { Label } from "../ui/label";
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
 
-type LoginFormFields = {
-  email: string;
-  password: string;
-}
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const schema = z.object({
+  email: z.email('Invalid email address'),
+  password: z.string().min(8, 'Password must be at least 8 characters'),
+});
+
+type LoginFormFields = z.infer<typeof schema>;
 
 export const Login = () => {
-  const { 
-    register, 
-    handleSubmit, 
-    setError, 
-    formState: { 
-      errors, 
-      isSubmitting 
-    } 
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
   } = useForm<LoginFormFields>({
     defaultValues: {
-      email: "test@email.com",
-      password: "",
+      email: 'test@email.com',
+      password: '',
     },
+    resolver: zodResolver(schema),
   });
 
   const onSubmit: SubmitHandler<LoginFormFields> = async (data) => {
     try {
       await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulate network delay
-      throw new Error(); // Simulate an error for demonstration   
-      console.log(data);   
+      // throw new Error();
+      console.log(data);
     } catch (error) {
-      setError("root", {
-        type: "manual",
-        message: "This email is already registered or invalid.",
+      setError('root', {
+        type: 'manual',
+        message: 'This email is already registered or invalid.',
       });
     }
-  }
+  };
 
   return (
-    <form className="max-w-sm w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
-      <div className="space-y-2">
-        <Label htmlFor="email">Email</Label>
-        <Input {...register("email", {
-          required: "Email is required",
-          pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-        })} type="email" id="email" name="email" />
+    <form
+      className='max-w-sm w-full space-y-4'
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className='space-y-2'>
+        <Label htmlFor='email'>Email</Label>
+        <Input {...register('email')} type='email' id='email' name='email' />
       </div>
-      {errors.email && 
-        (<div className="text-destructive">
+      {errors.email && (
+        <div className='text-destructive'>
           <span>{errors.email.message}</span>
-        </div>)
-      }
+        </div>
+      )}
 
-      <div className="space-y-2">
-        <Label htmlFor="password">Password</Label>
-        <Input {...register("password", {
-          required: "Password is required",
-          minLength: {
-            value: 6,
-            message: "Password must be at least 6 characters",
-          },
-        })} type="password" id="password" name="password" />
+      <div className='space-y-2'>
+        <Label htmlFor='password'>Password</Label>
+        <Input
+          {...register('password')}
+          type='password'
+          id='password'
+          name='password'
+        />
       </div>
-      {errors.password &&  
-        (<div className="text-destructive">
+      {errors.password && (
+        <div className='text-destructive'>
           <span>{errors.password.message}</span>
-        </div>)
-      }
+        </div>
+      )}
 
-      <button disabled={isSubmitting} type="submit" className="w-full bg-blue-500 text-white py-2 rounded">
-        {isSubmitting ? "Logging in..." : "Login"}
+      <button
+        disabled={isSubmitting}
+        type='submit'
+        className='w-full bg-blue-500 text-white py-2 rounded'
+      >
+        {isSubmitting ? 'Logging in...' : 'Login'}
       </button>
-      {errors.root &&  
-        (<div className="text-destructive">
+      {errors.root && (
+        <div className='text-destructive'>
           <span>{errors.root.message}</span>
-        </div>)
-      }
+        </div>
+      )}
     </form>
-  )
-  }
+  );
+};
